c9e24b598f3382ff3e768963d0c0f7fb3e82fa70
Yan Lyra <yan.lyra@growth.sale>
2023-11-04 14:49:18 -0300

    feat: added loading page to search

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,11 +1,28 @@
-import { Skeleton } from '../../../components/skeleton'
+'use client'
+
+import { Skeleton } from '@/components/skeleton'
+import { useSearchParams } from 'next/navigation'
 
 export default function SearchLoading() {
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get('q')
+
   return (
-    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
-      <Skeleton className="col-span-6 row-span-6 h-[856px]" />
-      <Skeleton className="col-span-3 row-span-3" />
-      <Skeleton className="col-span-3 row-span-3" />
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[405px]" />
+        <Skeleton className="h-[405px]" />
+        <Skeleton className="h-[405px]" />
+
+        <Skeleton className="h-[405px]" />
+        <Skeleton className="h-[405px]" />
+        <Skeleton className="h-[405px]" />
+      </div>
     </div>
   )
 }
